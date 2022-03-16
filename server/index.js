5c8808c0a3e6c26e45dd6353d414b35523797baf
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-03-16 17:29:22 +0000

    initial commit

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
 const db = require("./lib/in-memory-db");
+const morgan = require("morgan");
 
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
@@ -31,6 +32,8 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
+app.use(morgan);
+
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
