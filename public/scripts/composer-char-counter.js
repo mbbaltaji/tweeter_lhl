ea015be81bd1e16d4a949bfbf59ead4f551c92e8
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-03-21 21:23:25 +0000

    feat: implement dynamic character counter underneath compase tweet text area to display to user how many characters they can type in a single tweet. Turns red when char limit is reached

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+  // handler that counts and tweet chars and dynamically changes the <output> element
+  $('#tweet-text').on('input', function(e){
+    let charCount = 140;
+    let count = charCount - $(this).val().length; //$(this).val stores the string val in the textarea el
+    let output = $(this).next().children()[1];
+
+    //.next gets the next sibling of an element
+    // .children returns an array pf children
+    output.innerHTML = count; 
+    if (count < 0) {
+      $(output).css('color', 'red');
+    } else {
+      $(output).css('color', 'grey');
+    }
+  });
+});
